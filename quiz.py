af329a7b1fdf346b30a34c6189f4d83cd54cd06a
kamal koushik duppalapudi <kamalkoushik24@gmail.com>
2020-07-25 19:30:55 +0530

    commit

---
+++ quiz.py
@@ -0,0 +1,11 @@
+import math
+from os import sleep
+import cs50  #importing the required modules
+
+name = get_string("Hello there! What is your name?")
+
+points = 0
+
+ans1 =get_string("Who is the prime minister of India?")
+if ans1 == 'Narendra Modi'
+  print("correct an")
