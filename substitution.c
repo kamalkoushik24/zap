0ee1431c91361d21d03e93c00f8b43169e810eb6
kamal koushik duppalapudi <kamalkoushik24@gmail.com>
2020-07-25 19:27:51 +0530

    basic changes

--- substitution.c
+++ substitution.c
@@ -1,51 +1,24 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <cs50.h>
 #include <string.h>
-#include <ctype.h>
+#include <stdlib.h>
 
+bool key_validity(string key);
 
 int main(int argc, string argv[])
 {
-  if (argv != 2)
-  {
-    printf("Usage: ./substiution key\n");
-    return 1;
-  }
-  if (!key_validity(argv[1]))
-  {
-    printf("Key must contain 26 characters\n");
-  }
+	if (argc != 2)
+	{
+		printf("Usage: ./substitution key\n");
+	}
+    
+    string s = get_string("plaintext:  ")
+	string key = argv[1];
+	string alpha = abcdefghijklmnopqrstuvwxyz;
 
-  string s = get_string("plaintext:  ");
-  string diff = argv[1];
-  for (int i = 'A'; i < 'Z'; i++)
-      diff[i - 'A'] = toupper(diff[i - 'A']) - i;
 
-  printf("ciphertext: ");
-  for (int i = 0, len = strlen(s); i < len; i++)
-  {
-    if (isalpha(s[i]))
-        s[i] = s[i] + diff[s[i] - (isupper(s[i]) ? 'a' : 'A')];
-    printf("%c", s[i]);
-  }
-
+	
 }
 
-bool key_validity(string s)
-{
-     int len = strlen(s)
-     if (len != 26)
-         return false;
-
-     for (int i; i < len; i++)
-        if (!isalpha(s[i]))
-            return false;
-        int index = toupper(s[i]) - 'A'
-        if (freq[index] > 0)
-            return false;
-        freq[index]++;
-
-
-
-
-}
+bool key_validity(string key)
