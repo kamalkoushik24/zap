0ea7aafcb2a023c20e51b0ce0f8d3d30ff1bc5f6
kamal koushik duppalapudi <kamalkoushik24@gmail.com>
2020-08-12 16:48:24 +0530

    commit

--- Python Problems/credit.py
+++ Python Problems/credit.py
@@ -1,11 +1,15 @@
 from cs50 import get_int
+import sys
+
+
+
 
 def main():
     while True:
         ccn = get_int("Number: ")
         if ccn > 0:
           break
-    if (validity(ccn) = True):
+    if (validity(ccn) == True):
         credit_card_brand(ccn)
     else:
         print("INVALID")
@@ -25,10 +29,10 @@ def checksum(ccn):
 
         cc //= 10
 
-    sum % 10 == 0:
+    sum % 10 == 0
 
 def credit_card_brand(ccn):
-    if ((ccn >= 4e12 and ccn < 5e12) or (ccn >= 4e15 and ccn < 5e15)):
+  if ((ccn >= 4e12 and ccn < 5e12) or (ccn >= 4e15 and ccn < 5e15)):
     print("VISA")
 
   elif ((ccn >= 34e13 and ccn < 35e13) or (ccn >= 37e13 and ccn < 38e13)): # printing the brand of the credit card
@@ -41,4 +45,4 @@ def credit_card_brand(ccn):
     print("INVALID") # printing invalid if it is
 
 if __name__ == "__main__":
-main()
+  main()
